2fc7db31694f3616725a643d0e8b87a3b4d749ba
tahayllldz <tahayllldz@gmail.com>
2026-06-16 22:22:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = "8953886144:AAFQtQ904xQAv9NYrsOvP3Ruwx8RyDV5Ado"
+BOT_TOKEN = "8953886144:AAFe42xShCjWzoAfauueIb3nmX_aNXHdcpk"
 
 async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
